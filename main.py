304d0b362443685ed33d4289993e7c1ea76e2754
Vitaly Kichenko <vkichenko@gmail.com>
2025-07-01 15:54:22 +0300

    task1, 2

---
+++ main.py
@@ -0,0 +1,37 @@
+# Task 1
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n<= 0:
+            return 0
+        elif n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        if n <= 1:
+            return n
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    return fibonacci
+
+fib = caching_fibonacci()
+print(fib(10))  
+print(fib(15))  
+
+#Task 2
+import re
+def generator_numbers(text: str):
+    text_digits = re.findall(r' \d+\.\d+ | \d+ ', text) #space before and after numbers
+    for num in text_digits:
+        yield float(num)
+
+def sum_profit(text: str, generator_numbers: callable) -> float:
+    total = 0
+    for number in generator_numbers(text):
+        total += number
+    return total
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
