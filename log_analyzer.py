f013d7455e63c9d0a4dbd0279427f6370edec9a7
Vitaly Kichenko <vkichenko@gmail.com>
2025-07-04 03:04:10 +0300

    log analyzer done

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,9 +3,6 @@ import sys
 
 
 def parse_log_line(line: str) -> dict:
-    """
-    Parse a single line of the log file and return a dictionary with the parsed data.
-    """
     parts = line.strip().split()
     if len(parts) < 3:
         return None
@@ -15,14 +12,15 @@ def parse_log_line(line: str) -> dict:
         'level': parts[2],
         'message': ' '.join(parts[3:])
     }
+
 def filter_logs_by_level(logs: list, level: str) -> list:
-    return [log for log in logs if log['level'].lower() == level.lower()]
+    # variant1
+    return list(filter(lambda log: log['level'].upper() == level.upper(), logs))
+    # variant2
+    # return [log for log in logs if log['level'].upper() == level.upper()]   
+
 
 def count_logs_by_level(logs: list) -> dict:
-    """
-    Count the number of log entries for each log level.
-    Returns a dictionary with log levels as keys and counts as values.
-    """
     level_count = {}
     for log in logs:
         level = log['level']
@@ -41,12 +39,10 @@ def display_log_counts(counts: dict):
     print("-" * len(header))
     for level, count in counts.items():
         print(f"{level:<{len(header_field1)}} |{count:^{len(header_field2)}}")
-    print()  # Add a newline for better readability
+    print("-" * len(header))
+    print()  
 
 def load_logs(file_path: str) -> list:
-    """
-    Load the log file and return a list of dictionaries with parsed log entries.
-    """
     logs = []
     try:
         with open(file_path, 'r') as file:
@@ -58,21 +54,28 @@ def load_logs(file_path: str) -> list:
         print(f"Log file {file_path} not found.")
     return logs
 
-def main():
-    if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: python log_analyzer.py <log_file_path>")
-            sys.exit(0)
 
-        log_file_path = sys.argv[1]
-        logs = load_logs(log_file_path)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python log_analyzer.py <log_file_path> | <level>")
+        sys.exit(0)
 
+    log_file_path = sys.argv[1]
+    logs = load_logs(log_file_path)
+    log_stats = count_logs_by_level(logs)
+    display_log_counts(log_stats)
+
+    if not logs:
+        print("No logs found or unable to read the log file.")
+        sys.exit(0)
+
+    if len(sys.argv) > 2:
+        level = sys.argv[2]
+        filtered_log = filter_logs_by_level(logs, level)
         if not logs:
-            print("No logs found or unable to read the log file.")
+            print(f"No logs found for level: {level}")
             sys.exit(0)
-        log_stats = count_logs_by_level(logs)
-        display_log_counts(log_stats)
-        # for log in logs:
-            # print(f"{log['timestamp']} [{log['level']}] {log['message']}")
+        print(f"Деталі логів для рівня '{level}':")
+        for raw in filtered_log:
+            print(f"{raw['date']} {raw['time']} - {raw['message']}")
 
-main()
